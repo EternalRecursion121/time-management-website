a2bc9ee0b446baaaee9492b46824ea5d9fbeaa99
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-02-16 00:08:09 +0000

    fix?

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,11 @@
 import type { LayoutServerLoad } from "./$types";
 import { decode } from '@auth/core/jwt';
 import { AUTH_SECRET } from "$env/static/private";
+import { dev } from '$app/environment';
 
 export const load: LayoutServerLoad = async (event) => {
   return {
     session: await event.locals.getSession(),
-    token: await decode({token: event.cookies.get("next-auth.session-token"), secret:AUTH_SECRET})
+    token: await decode({token:  event.cookies.get(dev ? "next-auth.session-token": "__Secure-next-auth.session-token"), secret:AUTH_SECRET})
   }
 }
